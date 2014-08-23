8246c35c7774a637bd24651e059e6e0ec0a3f3a0
iulia-pasov <iulia.pasov@gmail.com>
2014-08-23 13:39:33 +0300

    Added degree character in weather widget

--- public/javascripts/weatherWidget.js
+++ public/javascripts/weatherWidget.js
@@ -1,8 +1,6 @@
 function buildWeatherWidget(){
-	var outerDiv = $('#weather');
+	var outerDiv = $('#LocalWeather');
 	var weatherJson;
-
-	var jsonData = [{"name":"LocalWeather","data":{"city":"Lyon","icon":"http://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Lion_place_Sathonay_Lyon.JPG/220px-Lion_place_Sathonay_Lyon.JPG","description":"Sky is clear","temp":"298.40"}}];
 
 	for (var i=0; i<jsonData.length;i++) {
 		var elmt = jsonData[i];
@@ -15,7 +13,7 @@ function buildWeatherWidget(){
 		var container = $('<div>').addClass('weatherWidgetContainer').prop('id', 'weatherWidgetContainer').appendTo(outerDiv);
 		var left = $('<div>').addClass('weatherLeft').appendTo(container);
 		$('<div>').addClass('icon').append($('<img>',{id:'weatherIcon',src:weatherJson.icon})).appendTo(left);
-		$('<div>').addClass('temp').text(weatherJson.temp).appendTo(left);
+		$('<div>').addClass('temp').text(Math.round(weatherJson.temp*100)/100 + '°C').appendTo(left);
 		
 		var right = $('<div>').addClass('weatherRight').appendTo(container);
 		$('<div>').addClass('descriptionTxt').text('Current Weather in '+weatherJson.city).appendTo(right);
