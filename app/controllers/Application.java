493b5c38837a1fca57989f02bd52245ebdd8c9ce
Andrei Ciortea <andrei.ciortea@gmail.com>
2014-08-28 13:56:11 +0200

    Added basic functionality for retrieving a Wikipedia page.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,23 @@
 package controllers;
 
 import play.*;
+import play.libs.F.Function;
+import play.libs.F.Promise;
+import play.libs.ws.WS;
+import play.libs.ws.WSResponse;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    public static Promise<Result> index(String path) {
+        return WS.url("http://en.wikipedia.org/" + path).get().map(
+                new Function<WSResponse, Result>() {
+                    public Result apply(WSResponse response) {
+                        return ok(response.getBody()).as("text/html");
+                    }
+                }
+            );
     }
 
 }
