c5ca3b0b6229260b2d70be885ad605a5bee24beb
Blakko <Blakko@Blakko-Portatile>
2014-08-28 21:29:56 +0200

    The timezone for the clock widget is taken from the server data

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,23 +10,32 @@ $(document).ready(function(){
 
 
 	function updateclock(){
-		var timeZone = "Europe/Moscow";
+		var timeZone;
+		// Getting the timezone from the server gathered data
+		for(var elmt in jsonData){
+			if(elmt.name == 'LocalTime'){
+				timeZone = elmt.data.timezone;
+			}
+		}
 		
-		var date = new Date();
-		var time = moment(date.toISOString());
-		
-		// Converting the time using the timeZone
-		var timeInfo = time.tz(timeZone);
-		var localTime = timeInfo.format('h:mm:ss a');
-		
-		var curdate = new Date(timeInfo.format('YYYY-MM-DD[T]HH:mm:ss'));
-		var hour_as_degree = ( curdate.getHours() + curdate.getMinutes()/60 ) / 12 * 360
-		var minute_as_degree = curdate.getMinutes() / 60 * 360
-		var second_as_degree = ( curdate.getSeconds() + curdate.getMilliseconds()/1000 ) /60 * 360
-		$hands.filter('.hour').css({transform: 'rotate(' + hour_as_degree + 'deg)' })
-		$hands.filter('.minute').css({transform: 'rotate(' + minute_as_degree + 'deg)' })
-		$hands.filter('.second').css({transform: 'rotate(' + second_as_degree + 'deg)' })
-		requestAnimationFrame(updateclock)
+		if( timeZone != undefined && timeZone != null ) {
+			
+			var date = new Date();
+			var time = moment(date.toISOString());
+			
+			// Converting the time using the timeZone
+			var timeInfo = time.tz(timeZone);
+			var localTime = timeInfo.format('h:mm:ss a');
+			
+			var curdate = new Date(timeInfo.format('YYYY-MM-DD[T]HH:mm:ss'));
+			var hour_as_degree = ( curdate.getHours() + curdate.getMinutes()/60 ) / 12 * 360
+			var minute_as_degree = curdate.getMinutes() / 60 * 360
+			var second_as_degree = ( curdate.getSeconds() + curdate.getMilliseconds()/1000 ) /60 * 360
+			$hands.filter('.hour').css({transform: 'rotate(' + hour_as_degree + 'deg)' })
+			$hands.filter('.minute').css({transform: 'rotate(' + minute_as_degree + 'deg)' })
+			$hands.filter('.second').css({transform: 'rotate(' + second_as_degree + 'deg)' })
+			requestAnimationFrame(updateclock)
+		}
 	}
 
 	requestAnimationFrame(updateclock)
